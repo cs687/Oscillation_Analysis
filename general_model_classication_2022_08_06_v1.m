e3bc3dfb37962cf49e204c1e287bb2d820778708
christian.schwall <cs687@cam.ac.uk>
2022-06-09 21:22:49 +0100

    Reorganized data to make it clear which are the important files!

--- general_model_classication_2022_08_06_v1.m
+++ general_model_classication_2022_08_06_v1.m
@@ -1,31 +1,39 @@
-function class_cell=general_model_classication_2022_03_01_v1(data_path,varargin);
+function [class_cell,class_cell_num]=general_model_classication_2022_08_06_v1(varargin);
+% This function classifies the experimental data JLB263. This is a cleanup
+% verison
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This function need the intput of data_path a cell array witht the data
+% locations.
+% example input data: 
+%data_path={'\\slcu.cam.ac.uk\Data\Microscopy\TeamJL\Chris\movies\oscillations\2021-11-15\subAuto\Data\',...
+%'\\slcu.cam.ac.uk\Data\Microscopy\TeamJL\Chris\movies\oscillations\2022-01-19\subAuto\Data\',...
+%'\\slcu.cam.ac.uk\Data\Microscopy\TeamJL\Chris\movies\oscillations\2022-01-26\subAuto\Data\'};
 
-% %out_path='\\slcu.cam.ac.uk\data\Microscopy\TeamJL\Chris\movies\oscillations\2021-11-15\subAuto\Data\';
-% to_do={'\\slcu.cam.ac.uk\data\Microscopy\TeamJL\Chris\movies\oscillations\2021-11-15\subAuto\Data\JLB254_0uM_IPTG.mat','\\slcu.cam.ac.uk\data\Microscopy\TeamJL\Chris\movies\oscillations\2021-11-15\subAuto\Data\JLB254_3uM_IPTG.mat','\\slcu.cam.ac.uk\data\Microscopy\TeamJL\Chris\movies\oscillations\2021-11-15\subAuto\Data\JLB254_4uM_IPTG.mat',...
-%     '\\slcu.cam.ac.uk\data\Microscopy\TeamJL\Chris\movies\oscillations\2021-11-15\subAuto\Data\JLB254_5uM_IPTG.mat','\\slcu.cam.ac.uk\data\Microscopy\TeamJL\Chris\movies\oscillations\2021-11-15\subAuto\Data\JLB254_6uM_IPTG.mat',...
-%     '\\slcu.cam.ac.uk\data\Microscopy\TeamJL\Chris\movies\oscillations\2021-11-15\subAuto\Data\JLB254_7uM_IPTG.mat','\\slcu.cam.ac.uk\data\Microscopy\TeamJL\Chris\movies\oscillations\2021-11-15\subAuto\Data\JLB254_8uM_IPTG.mat',...
-%     '\\slcu.cam.ac.uk\data\Microscopy\TeamJL\Chris\movies\oscillations\2021-11-15\subAuto\Data\JLB254_9uM_IPTG.mat'};
 
-%figure;
-%for i=2:length(to_do);
-
-
+%%%%%%%%%%%%%%%
 %Default parameters
-numlags=500;
-numstd=3;
-cond=1:8;
-plot_now=2;
-do_plot=0;
-plot_color=1;
-do_auto=0;
-L=576;
-lag=36;
-name={'0 uM IPTG','2uM IPTG','3uM IPTG','4uM IPTG','5 uM IPTG','6 uM IPTG','7 uM IPTG','8 uM IPTG','9 uM IPTG'};
-iptg=[0,2,3,4,5,6,7,8,9];
-on_colors_all={'w','y','g','c'};
-class_cell=zeros(4,length(iptg),length(data_path));
-class_cell_num=zeros(4,length(iptg),length(data_path));
+%%%%%%%%%%%%%%%%
+%input data
+data_path={'\\slcu.cam.ac.uk\Data\Microscopy\TeamJL\Chris\movies\oscillations\2021-11-15\subAuto\Data\',...
+'\\slcu.cam.ac.uk\Data\Microscopy\TeamJL\Chris\movies\oscillations\2022-01-19\subAuto\Data\',...
+'\\slcu.cam.ac.uk\Data\Microscopy\TeamJL\Chris\movies\oscillations\2022-01-26\subAuto\Data\'};
+plot_now=2; %decids which plot to do: 1 plots MY; 2 plots the autocorrelation and all else plots the frequencey analysis
+do_plot=0; %Do Quality control plots 0 no plot. 1 plots
+plot_color=1; %setting colour for quality control plots as in on_colors_all
+do_auto=0; %do autocorrelation for classification: 0 no autocorrelation 1 autcorrelation
+L=576; %number of frames anlaysed
 
+lag=36; %discarding the first 6h
+name={'0 uM IPTG','2uM IPTG','3uM IPTG','4uM IPTG','5 uM IPTG','6 uM IPTG','7 uM IPTG','8 uM IPTG','9 uM IPTG'};%name of conditions
+iptg=[0,2,3,4,5,6,7,8,9];%conditions in numbers
+on_colors_all={'w','y','g','c'};%colors to plot
+class_cell=zeros(4,length(iptg),length(data_path)); %setting output
+class_cell_num={};%setting output
+
+
+%%%%%%%%%%%%%%
+%Checking for variable intput
+%%%%%%%%%%%%
 for i=1:2:length(varargin)
     if strcmp(varargin{i},'numlag')
         numlags=varargin{i+1};
@@ -46,15 +54,19 @@ for i=1:2:length(varargin)
         L=varargin{i+1};
     elseif strcmp(varargin{i},'do_auto')
         do_auto=varargin{i+1};
+    elseif strcmp(varargin{i},'data_path')
+        data_path=varargin{i+1};
     end
 end
 
-for tt=1:length(data_path)
+%%%%%%%%%%%%%%%%%%
+% Classification
+%%%%%%%%%%%%%%%%%%%%%
+
+for tt=1:length(data_path) %loop over all input data
     kk=0;
-    for i=1:length(iptg)
+    for i=1:length(iptg) %loop over all iptg concentrations
         kk=kk+1;
-        %load data
-        %figure;
         D=dir([data_path{tt},'*',num2str(iptg(i)),'uM*.mat']);
         if ~isempty(D)
             load([data_path{tt},D(1).name]);
@@ -100,20 +112,11 @@ for tt=1:length(data_path)
                     [m2,loc2]=findpeaks(pd,'MinPeakProminence',10^6);
                 end
                 
-                
-%                 %calculating autocorrelation
-%                 [af,~,bounds]=autocorr(data-mean(data),'Numlags',numlags,'NumSTD',numstd);
-%         %         [loc_af,m_af]=peakfinder_2016(af);
-%                 [m_af,loc_af]=findpeaks(af,'MinPeakHeight',bounds(1));
-                %Caldulating periogram
-%                 af=data;
-%                 [pd,freq]=pwelch(af-mean(af),[],[],[],1/10);
-%                 [m2,loc2]=findpeaks(pd,'MinPeakProminence',1);
+                %Actual Classification
                 if sum((m-p)>1500)~=0% on case: magenta
                     color_now=on_colors_all{4};
                     class_cell(4,kk,tt)=class_cell(4,kk,tt)+1;
-                %elseif freq(loc2(1))>1/(5000/8); %oscillation case: green
-                elseif sum(loc2>1)>0 && sum(freq(loc2)>10^-3)>0
+                elseif sum(loc2>1)>0 && sum(freq(loc2)>10^-3)>0 %oscillation case
                 %elseif sum(loc_af>bounds(1))>2 && sum(freq(loc2)>10^-3)>0;
                     color_now=on_colors_all{3};
                     class_cell(3,kk,tt)=class_cell(3,kk,tt)+1;
@@ -153,17 +156,19 @@ for tt=1:length(data_path)
                     end
                 end
             end
-            %defining out put
+            %defining output
             if do_plot==1
                 title(name{i});
             else
+                class_cell_num{tt}(:,kk)=class_cell(:,kk,tt);
                 class_cell(:,kk,tt)=class_cell(:,kk,tt)/norm;
-                class_cell_num(:,kk,tt)=class_cell(:,kk,tt);
             end
         else
             class_cell(:,kk,tt)=nan(1,4);
         end
     end
 end
-% figure;
-% plot(mean(class_cell,3));
+figure;
+errorbar([iptg;iptg;iptg;iptg]',nanmean(class_cell,3)',nanstd(class_cell,0,3)');
+xlabel('IPTG (uM)');
+ylabel('Fraction (au)');
